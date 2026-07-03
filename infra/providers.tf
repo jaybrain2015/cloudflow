c833ef87072ed801d5621cdd8932ba375a85d0e2
john <johnjonah0866@gmail.com>
2026-07-03 14:38:10 +0300

    Move Terraform state to S3 backend with locking

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,9 +1,16 @@
 terraform {
+  backend "s3" {
+    bucket       = "cloudflow-tfstate-71ff924c"
+    key          = "cloudflow/terraform.tfstate"
+    region       = "eu-north-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 5.0"
-
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
